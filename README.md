55a92497d2477a832698162eeda4a559d2106be3
Sanjana Boddu <boddu.saa12@gmail.com>
2022-08-04 00:28:11 -0700

    Add nodemon, update schema, update gitignore.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # expressApp
 This app allows users to store Car information and retrieve it.
 It uses nodejs (ES6), Express, MySQL.
-Routes protected using JWT authorization.
+Routes protected using JWT authorization.\
+`npm run watch`
--- car_database.sql
+++ car_database.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2022 at 07:24 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Aug 04, 2022 at 09:06 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,9 +29,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cars` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `brand` varchar(255) NOT NULL
+                        `id` int(11) NOT NULL,
+                        `name` varchar(255) NOT NULL,
+                        `brand` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -38,10 +39,10 @@ CREATE TABLE `cars` (
 --
 
 INSERT INTO `cars` (`id`, `name`, `brand`) VALUES
-(1, 'maruti', 'suzuki'),
-(4, 'CRV', 'honda'),
-(5, 'corolla', 'toyota'),
-(6, 'corolla', 'toyota');
+                                               (1, 'maruti', 'suzuki'),
+                                               (4, 'CRV', 'honda'),
+                                               (5, 'corolla', 'toyota'),
+                                               (6, 'corolla', 'toyota');
 
 -- --------------------------------------------------------
 
@@ -50,10 +51,10 @@ INSERT INTO `cars` (`id`, `name`, `brand`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `registered` varchar(255) NOT NULL
+                         `id` int(11) NOT NULL,
+                         `email` varchar(255) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `registered` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -61,7 +62,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `registered`) VALUES
-(1, 'test@gmail.com', '$2b$10$l.O8wVZQBn/MRMYQw8GKruP24rf71LIHBtaGdCkrXU2GjxbQXhBzK', '0000-00-00');
+    (1, 'test@gmail.com', '$2b$10$l.O8wVZQBn/MRMYQw8GKruP24rf71LIHBtaGdCkrXU2GjxbQXhBzK', '1659596732');
 
 --
 -- Indexes for dumped tables
@@ -71,31 +72,23 @@ INSERT INTO `users` (`id`, `email`, `password`, `registered`) VALUES
 -- Indexes for table `cars`
 --
 ALTER TABLE `cars`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `cars`
---
-ALTER TABLE `cars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
